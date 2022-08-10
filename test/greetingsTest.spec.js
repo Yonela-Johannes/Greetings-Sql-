0dc450626e4186a3b9202f096c4e2c11c7e24217
Yonela-Johannes <johannesyonela@gmail.com>
2022-08-10 02:28:15 +0200

    All database crud tests.

--- test/greetingsTest.spec.js
+++ test/greetingsTest.spec.js
@@ -1,19 +1,12 @@
 const { Greet } = require('../app.js')
 const { assert } = require('chai');
-const { db,
-    updateuser,
-} = require('../config/db.js')
-
-
+const { db } = require('../config/db.js')
 describe('Greetings function Test', () => {
+    beforeEach(async () => {
+        await db.query("DELETE FROM greetings_test")
+    })
     describe("Empty Name Test", () => {
-        it('should not pass in empty name', () => {
-            before(async () => {
-                await greetings_test.destroy({
-                    where: {},
-                    truncate: { cascade: true },
-                })
-            })
+        it('should not pass in empty name', async () => {
             const greeting = Greet()
             const username = ''
             const languages = 'Afrikaans'
@@ -22,49 +15,18 @@ describe('Greetings function Test', () => {
             assert.equal("Please enter your name!", greeting.getNameError())
         });
     });
-    describe("Get Names", () => {
-        it('should get name from data base', async () => {
-            before(async () => {
-                await greetings_test.destroy({
-                    where: {},
-                    truncate: { cascade: true },
-                })
-                const greeting = Greet()
-                const username = ''
-                const languages = 'Afrikaans'
-                greeting.setName(username)
-                greeting.setGreeting(languages)
-                const allUsers = await db.query('SELECT * FROM greetings_test;')
-                assert.deepEqual([], allUsers)
-            })
-        });
-    });
-
-    describe("Empty Name Test", () => {
-        it('should not pass in empty name', () => {
-            before(async () => {
-                await greetings_test.destroy({
-                    where: {},
-                    truncate: { cascade: true },
-                })
-            })
+    describe("Get All Data", () => {
+        it('should get all data from data base', async () => {
             const greeting = Greet()
             const username = ''
             const languages = 'Afrikaans'
             greeting.setName(username)
             greeting.setGreeting(languages)
-            assert.equal("Please enter your name!", greeting.getNameError())
-        });
-
+            assert.deepEqual([], await db.query('SELECT * FROM greetings_test;'))
+        })
     });
     describe("Numbers Test", () => {
         it('should not pass in numbers', () => {
-            before(async () => {
-                await greetings_test.destroy({
-                    where: {},
-                    truncate: { cascade: true },
-                })
-            })
             const greeting = Greet()
             const username = 5555
             const languages = 'Afrikaans'
@@ -75,43 +37,20 @@ describe('Greetings function Test', () => {
     });
     describe("Empty Language Test", () => {
         it('should not pass in empty language', () => {
-            before(async () => {
-                await greetings_test.destroy({
-                    where: {},
-                    truncate: { cascade: true },
-                })
-            })
             const greeting = Greet()
             const username = 'Yonela'
             const languages = ''
 
             greeting.setName(username)
             greeting.setGreeting(languages)
-            // const getbased_name = username ? getuser_byname(username) : username
-            // if (getbased_name === greeting.getName()) {
-            //     if (greeting.getName(), greeting.getLanguage(), greeting.getSelectedLanguage(), greeting.getCount()) {
-            //         updateuser(getbased_name)
-            //     }
-            // } else {
-            //     if (greeting.getName(), greeting.getLanguage(), greeting.getSelectedLanguage(), greeting.getCount()) {
-            //         insertuser(greeting.getName(), greeting.getLanguage(), greeting.getSelectedLanguage(), greeting.getCount())
-            //     }
-            // }
             assert.equal("Please select language!", greeting.getLanguageError())
         });
     });
     describe("isiXhosa", () => {
         it('should return name and selected language', () => {
-            before(async () => {
-                await greetings_test.destroy({
-                    where: {},
-                    truncate: { cascade: true },
-                })
-            })
             const greeting = Greet()
             const username = 'Yonela'
             const languages = 'isiXhosa'
-
             greeting.setName(username)
             greeting.setGreeting(languages)
             const result = `${greeting.getGreeting()} ${greeting.getName()}`
@@ -120,12 +59,6 @@ describe('Greetings function Test', () => {
     });
     describe("Set Database Name", () => {
         it('should get user and user info by name.', async () => {
-            before(async () => {
-                await greetings_test.destroy({
-                    where: {},
-                    truncate: { cascade: true },
-                })
-            })
             const greeting = Greet()
             const username = 'Yonela'
             const languages = 'isiXhosa'
@@ -137,36 +70,52 @@ describe('Greetings function Test', () => {
             const language = greeting.getLanguage()
             const result = greeting.result()
             const [getbased_name] = await db.query(`SELECT * FROM greetings_test WHERE name = $1;`, [name])
-
-            if (getbased_name === name) {
-                if (result) {
-                    await db.query('UPDATE greetings_test  SET language=$2, greet=$3, count= count + 1 WHERE name = $1;', [name, language, greet])
-                } else {
-                    if (result) {
-                        await db.query(`INSERT INTO greetings_test (name, language, greet, count) VALUES ($1, $2, $3, $4);`, [name, language, greet, count])
-                    }
-                }
-                const get_user = await db.query(`SELECT * FROM greetings_test WHERE name = $1;`, [name])
-                const user = [{
-                    id: 5,
-                    name: 'Yonela',
-                    language: 'isiXhosa',
-                    greet: 'Molo',
-                    count: 1
-                }]
-
-                assert.deepEqual(user, get_user)
+            if (result && getbased_name?.name !== name) {
+                await db.query(`INSERT INTO greetings_test (name, language, greet, count) VALUES ($1, $2, $3, $4);`, [name, language, greet, count])
             }
+            const [answer] = await db.query(`SELECT * FROM greetings_test WHERE name = $1;`, [name])
+            const user = {
+                id: answer.id,
+                name: 'Yonela',
+                language: 'isiXhosa',
+                greet: 'Molo',
+                count: 1
+            }
+            assert.deepEqual(user, answer)
         });
     });
     describe("Update Database Name", () => {
         it('should get user and user info by name.', async () => {
-            before(async () => {
-                await greetings_test.destroy({
-                    where: {},
-                    truncate: { cascade: true },
-                })
-            })
+            const greeting = Greet()
+            const username = 'Okuhle'
+            const languages = 'Afrikaans'
+            greeting.setName(username)
+            greeting.setGreeting(languages)
+            const name = greeting.getName()
+            const greet = greeting.getGreeting()
+            const count = greeting.getCount()
+            const language = greeting.getLanguage()
+            const result = greeting.result()
+
+            const [getbased_name] = await db.query(`SELECT * FROM greetings_test WHERE name = $1;`, [name])
+            if (result && getbased_name?.name === name) {
+                await db.query('UPDATE greetings_test  SET language=$2, greet=$3, count= count + 1 WHERE name = $1;', [name, language, greet])
+            } else if (result && getbased_name?.name !== name) {
+                await db.query(`INSERT INTO greetings_test (name, language, greet, count) VALUES ($1, $2, $3, $4);`, [name, language, greet, count])
+            }
+            const [answer] = await db.query(`SELECT * FROM greetings_test WHERE name = $1;`, [name])
+            const user = {
+                id: answer.id,
+                name: 'Okuhle',
+                language: 'Afrikaans',
+                greet: 'Hallo',
+                count: 1
+            }
+            assert.deepEqual(user, answer)
+        });
+    });
+    describe("Delete One User", () => {
+        it('should delete only selected user.', async () => {
             const greeting = Greet()
             const username = 'Yonela'
             const languages = 'isiXhosa'
@@ -178,40 +127,25 @@ describe('Greetings function Test', () => {
             const language = greeting.getLanguage()
             const result = greeting.result()
             const [getbased_name] = await db.query(`SELECT * FROM greetings_test WHERE name = $1;`, [name])
-            if (getbased_name === name) {
-                if (result) {
-                    await db.query('UPDATE greetings_test  SET language=$2, greet=$3, count= count + 1 WHERE name = $1;', [name, language, greet])
-                }
-            } else {
-                if (result) {
-                    await db.query(`INSERT INTO greetings_test (name, language, greet, count) VALUES ($1, $2, $3, $4);`, [name, language, greet, count])
-                }
+            if (result && getbased_name?.name !== name) {
+                await db.query(`INSERT INTO greetings_test (name, language, greet, count) VALUES ($1, $2, $3, $4);`, [name, language, greet, count])
             }
-            let counter = 1
-            const [get_user] = await db.query(`SELECT * FROM greetings_test WHERE name = $1;`, [name])
-            const user = {
-                id: 11,
-                name: 'Yonela',
-                language: 'isiXhosa',
-                greet: 'Molo',
-                count: counter++
-            }
-            assert.deepEqual(user, get_user)
-        }
-        );
+            const [answer] = await db.query(`SELECT * FROM greetings_test WHERE name = $1;`, [name])
+            const id = answer.id
+            await db.query('DELETE FROM greetings_test WHERE id = $1;', [id])
+            assert.deepEqual([], await db.query("SELECT * FROM greetings_test;"))
+        });
+    });
+    describe("Delete All Data", () => {
+        it('should clear all data from database.', async () => {
+            assert.deepEqual([], await db.query('DELETE FROM greetings_test '))
+        });
     });
     describe("Afrikaans", () => {
         it('should return name and selected language', () => {
-            before(async () => {
-                await greetings_test.destroy({
-                    where: {},
-                    truncate: { cascade: true },
-                })
-            })
             const greeting = Greet()
             const username = 'Yonela'
             const languages = 'Afrikaans'
-
             greeting.setName(username)
             greeting.setGreeting(languages)
             const result = `${greeting.getGreeting()} ${greeting.getName()}`
@@ -220,16 +154,9 @@ describe('Greetings function Test', () => {
     });
     describe("English", () => {
         it('should return name and selected language', () => {
-            before(async () => {
-                await greetings_test.destroy({
-                    where: {},
-                    truncate: { cascade: true },
-                })
-            })
             const greeting = Greet()
             const username = 'Yonela'
             const languages = 'English'
-
             greeting.setName(username)
             greeting.setGreeting(languages)
             const result = `${greeting.getGreeting()} ${greeting.getName()}`
@@ -238,16 +165,9 @@ describe('Greetings function Test', () => {
     });
     describe("Name Count", () => {
         it('should not count when name is not set', () => {
-            before(async () => {
-                await greetings_test.destroy({
-                    where: {},
-                    truncate: { cascade: true },
-                })
-            })
             const greeting = Greet()
             const username = ''
             const languages = 'Afrikaans'
-
             greeting.setName(username)
             greeting.setGreeting(languages)
             assert.equal(0, greeting.getCount())
@@ -255,19 +175,12 @@ describe('Greetings function Test', () => {
     });
     describe("Language Count", () => {
         it('should not count when language is not set', () => {
-            before(async () => {
-                await greetings_test.destroy({
-                    where: {},
-                    truncate: { cascade: true },
-                })
-            })
             const greeting = Greet()
             const username = 'Yonela'
             const languages = 'Afrikaans'
-
             greeting.setName(username)
             greeting.setGreeting(languages)
             assert.equal(0, greeting.getCount())
         });
-    });
-})
+    })
+});
