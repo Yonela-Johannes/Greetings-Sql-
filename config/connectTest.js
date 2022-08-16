1d70da52d5c843bc3aa07dea3f64883a041985ef
Yonela-Johannes <johannesyonela@gmail.com>
2022-08-17 01:03:43 +0200

    tests

--- config/connectTest.js
+++ config/connectTest.js
@@ -4,10 +4,7 @@ const local = 'postgres://postgres:juanesse123@localhost:5432/greetings_tests';
 const connectionString = process.env.DATABASE_URL || local
 const config = {
     connectionString,
-    max: 20,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    max: 20
 }
 
 const db = pgp(config)
